d5d993c055e7f10f5a94e3edbe5fbea9e637367d
Dujuniorrr <durvaljunior117@gmail.com>
2024-02-13 19:51:21 -0300

    [feat] add user DAO contract

---
+++ app/src/main/java/com/ifbaiano/powermap/dao/contracts/UserDao.java
@@ -0,0 +1,17 @@
+package com.ifbaiano.powermap.dao.contracts;
+
+import com.ifbaiano.powermap.model.User;
+
+import java.util.ArrayList;
+
+public interface UserDao{
+    User add(User user);
+    User edit(User user);
+    Boolean remove(User user);
+    ArrayList<User> findOne(String id);
+    ArrayList<User>  findAll();
+    ArrayList<User>  findAllClients();
+    ArrayList<User>  findAllAdmins();
+    Boolean changePassword(User user, String id);
+
+}
