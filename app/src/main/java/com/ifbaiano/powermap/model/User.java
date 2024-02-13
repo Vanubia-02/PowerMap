0ec9a8f8619169ebb2d4e4393bdb3bd2d1bcff27
Dujuniorrr <durvaljunior117@gmail.com>
2024-02-13 17:59:31 -0300

    [feat] add user model

---
+++ app/src/main/java/com/ifbaiano/powermap/model/User.java
@@ -0,0 +1,59 @@
+package com.ifbaiano.powermap.model;
+import com.google.firebase.firestore.Exclude;
+
+import java.io.Serializable;
+public class User implements Serializable {
+
+    @Exclude
+    private String id;
+
+    private String name, email, password, imgpath;
+    private boolean isAdmin;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImgpath() {
+        return imgpath;
+    }
+
+    public void setImgpath(String imgpath) {
+        this.imgpath = imgpath;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.isAdmin = admin;
+    }
+}
