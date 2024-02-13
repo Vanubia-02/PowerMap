4fa688d4770459bbd420872d8bfceb23ef871bc9
Dujuniorrr <durvaljunior117@gmail.com>
2024-02-13 18:03:42 -0300

    [feat] add schedule model

---
+++ app/src/main/java/com/ifbaiano/powermap/model/Schedule.java
@@ -0,0 +1,56 @@
+package com.ifbaiano.powermap.model;
+import com.google.firebase.firestore.Exclude;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Schedule implements  Serializable{
+    @Exclude
+    private String id;
+    private Date date;
+    private String description;
+    private Integer dayOfWeek, repetition;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(Integer dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public Integer getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(Integer repetition) {
+        this.repetition = repetition;
+    }
+
+
+
+}
